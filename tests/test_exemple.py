df48c3c7efa0274bf9b97b2d955f6a871d75c3cd
edusouza455 <edu.testes456@gmail.com>
2026-04-01 09:23:37 -0400

    ajustes de testes com novo modelo

--- tests/test_exemple.py
+++ tests/test_exemple.py
@@ -1,10 +1,16 @@
 import pytest
+import allure
 from playwright.sync_api import Page, expect
 from pages.login.exemple_base import LoginPage
 from utils.config import Config
 
+@allure.epic("Autenticação")
+@allure.feature("Login")
 class TestLogin:
     
+    @allure.story("Login com credenciais válidas")
+    @allure.title("Validar login com sucesso no OrangeHRM")
+    @allure.severity(allure.severity_level.BLOCKER)
     def test_successful_login(self, page: Page):
         # Dado que o usuário acessa a página de login do OrangeHRM
         # E preenche o usuário e senha com dados válidos
